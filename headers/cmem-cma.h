249c21511059ae7c8b78e4a522131985df9a392d
Mario Shehu <mario.shehu.07@gmail.com>
2026-08-02 15:56:14 +0200

    No hard limit of  16 buffers - one platform device per NUMA node (#1)
    * [buffers] Removed MAX_BUFFERS, added CMEM_CMA_HARD_MAX_BUFFERS. The minimum size of a buffer is PAGE_SIZE, and the effective maximum amount of buffers is the Driver allocation size divided by the minimum buffer size.
    * [platform device] One platform device for each NUMA node allocated into an xarray
    * [capabilities] Added check for CAP_SYS_RAWIO to allocate a buffer
    Add proc_ops.
    * Use pr_*_ratelimited in functions that can be called many times.
    * mmap_offset instead of user_addr
    * [Makefile] Fix install

--- headers/cmem-cma.h
+++ headers/cmem-cma.h
@@ -4,17 +4,17 @@
 #include <linux/ioctl.h>
 #include <linux/types.h>
 
-#define DEVICE_NAME "cmem_cma"   /**< Device name as seen in /dev */
-#define CLASS_NAME "cmem_cma"    /**< Device class name */
-#define MAX_BUFFERS 16           /**< Maximum number of concurrently allocated buffers */
+#define DEVICE_NAME "cmem_cma" /**< Device name as seen in /dev */
+#define CLASS_NAME "cmem_cma"  /**< Device class name */
 
 /**
  * @def CMEM_CMA_IOC_MAGIC
  * @brief IOCTL magic number used for CMA DMA commands
  *
- * 'C' is chosen arbitrarily but it simply must be unique among all IOCTL command groups.
- * 'C' is used by soundcard.h or capi.h, which should not create any problems.
- * See: https://www.kernel.org/doc/html/latest/userspace-api/ioctl/ioctl-number.html
+ * 'C' is chosen arbitrarily but it simply must be unique among all IOCTL
+ * command groups. 'C' is used by soundcard.h or capi.h, which should not create
+ * any problems. See:
+ * https://www.kernel.org/doc/html/latest/userspace-api/ioctl/ioctl-number.html
  */
 #define CMEM_CMA_IOC_MAGIC 'C'
 
@@ -40,14 +40,15 @@
  * @struct cmem_cma_alloc_req
  * @brief Request/response structure for DMA buffer allocation
  *
- * Userspace initializes `size` and optionally `numa_node`. Kernel fills in the rest.
+ * Userspace initializes `size` and optionally `numa_node`. Kernel fills in the
+ * rest.
  */
 struct cmem_cma_alloc_req {
-    size_t size;              /**< Requested size of the DMA buffer (in bytes) */
-    int numa_node;            /**< NUMA node to allocate from, or -1 for default */
-    __u64 dma_addr;           /**< (Out) DMA address (physical) of the allocated buffer */
-    unsigned long user_addr;  /**< (Out) User-space accessible virtual address */
-    int buffer_id;            /**< (Out) Internal ID of the allocated buffer */
+  size_t size;    /**< Requested size of the DMA buffer (in bytes) */
+  int numa_node;  /**< NUMA node to allocate from, or -1 for default */
+  __u64 dma_addr; /**< (Out) DMA address (physical) of the allocated buffer */
+  unsigned long mmap_offset; /**< (Out) buffer_id * PAGE_SIZE */
+  int buffer_id;             /**< (Out) Internal ID of the allocated buffer */
 };
 
 /**
@@ -55,7 +56,7 @@ struct cmem_cma_alloc_req {
  * @brief Request structure for DMA buffer deallocation
  */
 struct cmem_cma_free_req {
-    int buffer_id;            /**< ID of the buffer to free */
+  int buffer_id; /**< ID of the buffer to free */
 };
 
 /**
@@ -63,9 +64,9 @@ struct cmem_cma_free_req {
  * @brief Information structure returned by the CMEM_CMA_GET_INFO ioctl
  */
 struct cmem_cma_info {
-    int num_buffers;          /**< Current number of allocated buffers */
-    size_t total_allocated;   /**< Total allocated memory (in bytes) */
-    int numa_nodes_available; /**< Number of NUMA nodes detected by the kernel */
+  int num_buffers;          /**< Current number of allocated buffers */
+  size_t total_allocated;   /**< Total allocated memory (in bytes) */
+  int numa_nodes_available; /**< Number of NUMA nodes detected by the kernel */
 };
 
 #endif /* CMEM_CMA_H */
